78d5d75f8ba328d744186566f7e7706ff6e55928
Michael Hirsch <scienceopen@users.noreply.github.com>
2014-05-12 15:09:10 -0400

    Create lineClipping.py
    fork from https://bitbucket.org/marcusva/py-sdl2 (which has public-domain license)

---
+++ lineClipping.py
@@ -0,0 +1,74 @@
+# forked from https://bitbucket.org/marcusva/py-sdl2 (which has public-domain license)
+# Michael Hirsch May 2014
+# reference: http://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
+# I have corrected errors in the cohensutherland code and compared cohensutherland with Matlab polyxpoly() results.
+
+def cohensutherland(xmin, ymax, xmax, ymin, x1, y1, x2, y2,dbglvl=0):
+    """Clips a line to a rectangular area.
+
+    This implements the Cohen-Sutherland line clipping algorithm.  xmin,
+    ymax, xmax and ymin denote the clipping area, into which the line
+    defined by x1, y1 (start point) and x2, y2 (end point) will be
+    clipped.
+
+    If the line does not intersect with the rectangular clipping area,
+    four None values will be returned as tuple. Otherwise a tuple of the
+    clipped line points will be returned in the form (cx1, cy1, cx2, cy2).
+    """
+    INSIDE,LEFT, RIGHT, LOWER, UPPER = 0,1, 2, 4, 8
+
+    def _getclip(xa, ya,dbglvl=0):
+        if dbglvl>1: print('point: '),; print(xa,ya)
+        p = INSIDE  #default is inside
+
+        # consider x
+        if xa < xmin:
+            p |= LEFT
+        elif xa > xmax:
+            p |= RIGHT
+
+        # consider y    
+        if ya < ymin:
+            p |= LOWER # bitwise OR
+        elif ya > ymax:
+            p |= UPPER #bitwise OR
+        return p
+
+# check for trivially outside lines
+    k1 = _getclip(x1, y1,dbglvl)
+    k2 = _getclip(x2, y2,dbglvl)
+
+# examine non-trivially outside points
+    i = 0
+    while (k1 | k2) != 0: # if both points are inside box (0000) , ACCEPT trivial whole line in box
+
+        # if line trivially outside window, REJECT
+        if (k1 & k2) != 0:
+            if dbglvl>1: print('  REJECT trivially outside box')
+            return None, None, None, None
+            
+        #non-trivial case, at least one point outside window
+        opt = k1 or k2 # take first non-zero point, short circuit logic
+        if opt & UPPER:
+            x = x1 + (x2 - x1) * (1.0 * (ymax - y1)) / (y2 - y1) #1.0 forces float in divide
+            y = ymax
+        elif opt & LOWER:
+            x = x1 + (x2 - x1) * (1.0 * (ymin - y1)) / (y2 - y1)
+            y = ymin
+        elif opt & RIGHT:
+            y = y1 + (y2 - y1) * (1.0 * (xmax - x1)) / (x2 - x1)
+            x = xmax
+        elif opt & LEFT:
+            y = y1 + (y2 - y1) * (1.0 * (xmin - x1)) / (x2 - x1)
+            x = xmin
+        else: raise RuntimeError('Undefined clipping state')
+            
+        if opt == k1:
+            x1, y1 = x, y
+            k1 = _getclip(x1, y1,dbglvl)
+            if dbglvl>1: print('checking k1: ' + str(x) + ',' + str(y) + '    ' + str(k1))
+        elif opt == k2:
+            if dbglvl>1: print('checking k2: ' + str(x) + ',' + str(y) + '    ' + str(k2))
+            x2, y2 = x, y
+            k2 = _getclip(x2, y2,dbglvl)
+    return x1, y1, x2, y2

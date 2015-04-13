f1a81fd744f9db4824b75e4e47457f5553b4beeb
nucl <hirsch617@gmail.com>
2015-04-12 22:50:16 -0400

    incr coverage

--- lineClipping.py
+++ lineClipping.py
@@ -90,7 +90,12 @@ if __name__ == '__main__': #test case
     make box with corners LL/UR (1,3) (4,5)
     and line segment with ends (0,0) (4,6)
     '''
+#%% LL to UR test
     x1, y1, x2, y2 = cohensutherland(1,  5, 4, 3,
                                      0,  0, 4, 6)
 
-    assert_array_almost_equal([x1,y1,x2,y2],[2,3,3.3333333333333,5])
+    assert_array_almost_equal([x1,y1,x2,y2],[2, 3, 3.3333333333333,5])
+#%% LR to UL test
+    x1,y1,x2,y2 = cohensutherland(1,5,4,3,
+                                  5,0,0,6)
+    assert_array_almost_equal([x1,y1,x2,y2],[2.5, 1, 3, 4.8])

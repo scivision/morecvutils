2e6b2f609b9f1c5ae335543c6c67d2dc15112a00
scienceopen <scienceopen@users.noreply.github.com>
2015-05-11 16:47:03 -0400

    self test

--- lineClipping.py
+++ lineClipping.py
@@ -84,22 +84,3 @@ def cohensutherland(xmin, ymax, xmax, ymin, x1, y1, x2, y2):
             k2 = _getclip(x2, y2)
     return x1, y1, x2, y2
 
-if __name__ == '__main__': #test case
-    from numpy.testing import assert_array_almost_equal
-    '''
-    make box with corners LL/UR (1,3) (4,5)
-    and line segment with ends (0,0) (4,6)
-    '''
-#%% LOWER to UPPER test
-    x1, y1, x2, y2 = cohensutherland(1,  5, 4, 3,
-                                     0,  0, 4, 6)
-
-    assert_array_almost_equal([x1,y1,x2,y2],[2, 3, 3.3333333333333,5])
-#%% no intersection test
-    x1,y1,x2,y2 = cohensutherland(1,5,  4,3,
-                                  0,0.1,0,0.1)
-    assert x1==y1==x2==y2==None
-#%% left to right test
-    x1,y1,x2,y2 = cohensutherland(1,5,4,3,
-                                  0,4,5,4)
-    assert_array_almost_equal([x1,y1,x2,y2],[1, 4, 4, 4])

304bf50c9b45d2e422155605d551a18dd4cb963c
scienceopen <scienceopen@users.noreply.github.com>
2015-05-29 02:22:00 -0400

    init horn schunck

---
+++ calcOptFlow.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Michael Hirsch
+Example calculations of optical flow, starting with Horn Schunk Optical Flow using OpenCV
+"""
+
+import cv2
+from cv2 import cv
+from numpy import asarray,dstack,uint8
+
+def optflowHornSchunk(new,ref,uv,smoothing):
+    """
+    http://docs.opencv.org/modules/legacy/doc/motion_analysis.html
+    ***************************
+    Note that smoothness parameter for cv.CalcOpticalFlowHS needs to be SMALLER than matlab
+    to get similar result. Useless when smoothness was 1 in python, but it's 1 in Matlab!
+    *****************************
+    """
+    cvref = cv.fromarray(ref)
+    cvgray = cv.fromarray(new)
+    #result is placed in u,v
+    # matlab vision.OpticalFlow Horn-Shunck has default maxiter=10, terminate=eps, smoothness=1
+    cv.CalcOpticalFlowHS(cvref, cvgray, False, uv[0], uv[1],
+                         smoothing,
+                         (cv.CV_TERMCRIT_ITER | cv.CV_TERMCRIT_EPS, 8, 0.1))
+
+    # reshape to numpy float32, xpix x ypix x 2
+    return dstack((asarray(uv[0]), asarray(uv[1])))
+
+def setupuv(xpix,ypix):
+    """
+    Horn Schunck legacy OpenCV function requires we use these old-fashioned cv matrices, not numpy array
+    """
+    u =   cv.CreateMat(ypix, xpix, cv.CV_32FC1)
+    v =   cv.CreateMat(ypix, xpix, cv.CV_32FC1)
+    return (u, v)
+#%% demo
+if __name__ == '__main__':
+    from numpy.random import rand
+    xpix,ypix = 512,512
+    uv = setupuv(xpix,ypix)
+    old = (rand(ypix,xpix)*255).astype(uint8)
+
+    for i in range(100):
+        new = (rand(ypix,xpix)*255).astype(uint8)
+        flow = optflowHornSchunk(new,old,uv,smoothing=0.01)
+        old = new.copy()

142f1254cc9145c7bda9b55d08270dd4ed9f6b4e
scivision <scivision@users.noreply.github.com>
2015-08-02 16:17:11 -0400

    travis fallthrough

--- CVutils/__init__.py
+++ CVutils/__init__.py
@@ -1,1 +1,7 @@
-from . import (calcOptFlow,connectedComponents,cv2draw,lineClipping)
+from . import (lineClipping)
+
+try:
+    from . import (calcOptFlow,connectedComponents,cv2draw)
+except Exception as e:
+    from warnings import warn
+    warn('Possible not working OpenCV:   '.format(e))

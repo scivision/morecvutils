dbea7099912e60a214b626a5867dda77e07b96a5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-06-28 18:55:14 -0400

    cleanup api
    vers

--- morecvutils/getaviprop.py
+++ morecvutils/getaviprop.py
@@ -6,52 +6,44 @@ input: filename or cv2.Capture
 """
 from pathlib import Path
 from struct import pack
+from typing import Dict, Any
 import cv2
-try:
-    from cv2 import cv
-except ImportError:
-    cv = None  # OpenCV 3
 
 
-def getaviprop(f):
-    if isinstance(f, (str, Path)):  # assuming filename
-        f = Path(f).expanduser()
-        v = cv2.VideoCapture(str(f))
+def getaviprop(fn: Path) -> Dict[str, Any]:
+    if isinstance(fn, (str, Path)):  # assuming filename
+        fn = Path(fn).expanduser()
+        if not fn.is_file():
+            raise FileNotFoundError(fn)
+        v = cv2.VideoCapture(str(fn))
         if v is None:
-            raise RuntimeError('could not read {}'.format(f))
+            raise OSError(f'could not read {fn}')
     else:  # assuming cv2.VideoCapture object
-        v = f
+        v = fn
 
     if not v.isOpened():
-        raise RuntimeError('cannot read {}  probable codec issue'.format(f))
+        raise OSError(f'cannot read {fn}  probable codec issue')
 
-# %% note the subtle different CV_ prefix to property name, thus this verbose technique
-    if cv is not None:  # opencv 2.X
-        vidparam = {'nframe': int(v.get(cv.CV_CAP_PROP_FRAME_COUNT)),
-                    'xpix': int(v.get(cv.CV_CAP_PROP_FRAME_WIDTH)),
-                    'ypix': int(v.get(cv.CV_CAP_PROP_FRAME_HEIGHT)),
-                    'fps': v.get(cv.CV_CAP_PROP_FPS),
-                    'codec': fourccint2ascii(int(v.get(cv.CV_CAP_PROP_FOURCC)))
-                    }
-    else:  # opencv 3.0
-        vidparam = {'nframe': int(v.get(cv2.CAP_PROP_FRAME_COUNT)),
-                    'xpix': int(v.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                    'ypix': int(v.get(cv2.CAP_PROP_FRAME_HEIGHT)),
-                    'fps': v.get(cv2.CAP_PROP_FPS),
-                    'codec': fourccint2ascii(int(v.get(cv2.CAP_PROP_FOURCC)))
-                    }
-    if isinstance(f, Path):  # not if it was fed a capture!
+    vidparam = {'nframe': int(v.get(cv2.CAP_PROP_FRAME_COUNT)),
+                'xy_pixel': (int(v.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                             int(v.get(cv2.CAP_PROP_FRAME_HEIGHT))),
+                'fps': v.get(cv2.CAP_PROP_FPS),
+                'codec': fourccint2ascii(int(v.get(cv2.CAP_PROP_FOURCC)))
+                }
+
+    if isinstance(fn, Path):
         v.release()
 
     return vidparam
 
 
-def fourccint2ascii(fourcc_int):
+def fourccint2ascii(fourcc_int: int) -> str:
     """
-    useful for converting fourcc in integer form (32-bit int) to ASCII
+    convert fourcc 32-bit integer code to ASCII
     """
     assert isinstance(fourcc_int, int)
-    return pack('<I', fourcc_int)
+
+    return pack('<I', fourcc_int).decode('ascii')
 
 
 if __name__ == '__main__':
